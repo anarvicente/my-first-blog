c3b9dee5fdf0739c652f7eba25a310fc7c70ef09
Pedagogia EC <Pedagogia EC>
2017-04-22 02:21:06 -0300

    add comentarios nos posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post) #para ficar visivel na administracao
+admin.site.register(Comment)
